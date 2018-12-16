4a7e9c781865b465df0399162d31b7207ef8cf30
Cruise Brantley <cruise.brantley@gmail.com>
2018-12-15 19:16:51 -0600

    Options setting correct memory values

---
+++ options.js
@@ -0,0 +1,64 @@
+let page = document.getElementById("buttonDiv");
+let options = {};
+
+function constructOptions() {
+  chrome.storage.local.get("options", data => {
+    console.log(data.options);
+    data.hasOwnProperty("options") ? null : (data.options = {});
+
+    !data.options.hasOwnProperty("alignCells")
+      ? (data.options.alignCells = true)
+      : null;
+    const alignCellsText = document.createElement("span");
+    alignCellsText.innerHTML = "Move G-values to leftmost cell.";
+    const alignCellsCheckbox = createCheckbox(
+      "alignCells",
+      data.options.alignCells
+    );
+
+    !data.options.hasOwnProperty("timeout")
+      ? (data.options.timeout = true)
+      : null;
+    const timeoutText = document.createElement("span");
+    timeoutText.innerHTML = "Remove timeout from Apple notifications";
+    const timeoutCheckbox = createCheckbox("timeout", data.options.timeout);
+
+    !data.options.hasOwnProperty("autoCapitalize")
+      ? (data.options.autoCapitalize = true)
+      : null;
+    const autoCapitalizeText = document.createElement("span");
+    autoCapitalizeText.innerHTML = "Auto-capitalize KBB and KGB values";
+    const autoCapitalizeCheckbox = createCheckbox(
+      "autoCapitalize",
+      data.options.autoCapitalize
+    );
+
+    page.appendChild(alignCellsCheckbox);
+    page.appendChild(alignCellsText);
+    page.appendChild(document.createElement("br"));
+
+    page.appendChild(timeoutCheckbox);
+    page.appendChild(timeoutText);
+    page.appendChild(document.createElement("br"));
+
+    page.appendChild(autoCapitalizeCheckbox);
+    page.appendChild(autoCapitalizeText);
+    page.appendChild(document.createElement("br"));
+  });
+}
+function createCheckbox(optionName, optionValue) {
+  options[optionName] = optionValue;
+  optionValue === undefined ? (optionValue = true) : null;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = optionValue;
+  checkbox.addEventListener("change", () => {
+    options[optionName] = checkbox.checked;
+    chrome.storage.local.set({ options }, () => {
+      console.log(`Set ${optionName} to ${checkbox.checked}.`);
+    });
+  });
+  return checkbox;
+}
+
+constructOptions();
