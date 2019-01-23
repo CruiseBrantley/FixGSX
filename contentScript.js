b463a7cca3cc2eae0aa77ed59746753f1745f7fd
Cruise Brantley <cruise.brantley@gmail.com>
2019-01-23 16:37:38 -0600

    Fixed bug with options not being automatically set, and minor refactor

--- contentScript.js
+++ contentScript.js
@@ -16,109 +16,104 @@ function getData() {
         autoCapitalize: true,
         removeAutoLogout: true
       };
-      chrome.storage.local.set({ options });
+      chrome.storage.local.set({ options: data.options });
     } else {
-      autoCapitalizeFunction(data);
-      removeConfirmWaitFunction(data);
-      moveDispatchIDFunction(data);
-      removeAutoLogout(data);
+      if (
+        document.querySelector(
+          'input[name="repairDetails.repairOrderDetails[0].serialNumber"]'
+        ) &&
+        data.options.autoCapitalize
+      )
+        autoCapitalizeFunction(data);
+      if (document.querySelector(".link") != null && data.options.timeout)
+        removeConfirmWaitFunction(data);
+      if (
+        document.querySelector("#arrived_order_report") != null &&
+        data.options.alignCells
+      )
+        moveDispatchIDFunction(data);
+      if (data.options.removeAutoLogout && !hasRunOnce) removeAutoLogout(data);
       hasRunOnce = true;
     }
   });
 }
 
 function removeAutoLogout(data) {
-  if (data.options.removeAutoLogout && !hasRunOnce) {
-    function disablerAutoLogout() {
-      const legacyConfirm = window.confirm;
-      window.confirm = function confirm(msg) {
-        if (msg.startsWith("Your session will timeout")) {
-          const time = new Date().toLocaleTimeString();
-          console.log("FixGSX prevented automatic logout at", time);
-          return true; /*simulates user clicking yes*/
-        }
-        return legacyConfirm(msg);
-      };
-    }
-    let disablerAutoLogoutString = "(" + disablerAutoLogout.toString() + ")();";
-    let disablerScriptElement = document.createElement("script");
-    disablerScriptElement.textContent = disablerAutoLogoutString;
-    document.documentElement.appendChild(disablerScriptElement);
-    disablerScriptElement.parentNode.removeChild(disablerScriptElement);
+  function disablerAutoLogout() {
+    const legacyConfirm = window.confirm;
+    window.confirm = function confirm(msg) {
+      if (msg.startsWith("Your session will timeout")) {
+        const time = new Date().toLocaleTimeString();
+        console.log("FixGSX prevented automatic logout at", time);
+        return true; /*simulates user clicking yes*/
+      }
+      return legacyConfirm(msg);
+    };
   }
+  let disablerAutoLogoutString = "(" + disablerAutoLogout.toString() + ")();";
+  let disablerScriptElement = document.createElement("script");
+  disablerScriptElement.textContent = disablerAutoLogoutString;
+  document.documentElement.appendChild(disablerScriptElement);
+  disablerScriptElement.parentNode.removeChild(disablerScriptElement);
 }
 
 function moveDispatchIDFunction(data) {
+  const table = document.querySelector("#arrived_order_report").children[0]
+    .children[1];
+  const tableHead = table.children[1].children[0];
+  const tableBody = table.children[3];
+  const searchBox = document.querySelector("#global_search");
+  if (tableHead.children[10].innerText === "Dispatch ID\n") {
+    [tableHead.children[0].innerHTML, tableHead.children[10].innerHTML] = [
+      tableHead.children[10].innerHTML,
+      tableHead.children[0].innerHTML
+    ]; //swap the table Headers
+  }
   if (
-    document.querySelector("#arrived_order_report") != null &&
-    data.options.alignCells
+    tableBody.children.length > 0 &&
+    tableHead.children[0].innerText === "Dispatch ID\n" &&
+    tableBody.children[0].cells[10].innerText.charAt(0) === "G"
   ) {
-    const table = document.querySelector("#arrived_order_report").children[0]
-      .children[1];
-    const tableHead = table.children[1].children[0];
-    const tableBody = table.children[3];
-    const searchBox = document.querySelector("#global_search");
-    if (tableHead.children[10].innerText === "Dispatch ID\n") {
-      [tableHead.children[0].innerHTML, tableHead.children[10].innerHTML] = [
-        tableHead.children[10].innerHTML,
-        tableHead.children[0].innerHTML
-      ]; //swap the table Headers
-    }
-    if (
-      tableBody.children.length > 0 &&
-      tableHead.children[0].innerText === "Dispatch ID\n" &&
-      tableBody.children[0].cells[10].innerText.charAt(0) === "G"
-    ) {
-      for (let i = 0; i < tableBody.children.length; i++) {
-        [
-          tableBody.children[i].cells[0].innerHTML,
-          tableBody.children[i].cells[10].innerHTML
-        ] = [
-          tableBody.children[i].cells[10].innerHTML,
-          tableBody.children[i].cells[0].innerHTML
-        ]; //swap the table body elements
-        tableBody.children[i].addEventListener("click", () => {
-          searchBox.value = tableBody.children[i].cells[0].innerText;
-          searchBox.focus();
-        });
-      }
+    for (let i = 0; i < tableBody.children.length; i++) {
+      [
+        tableBody.children[i].cells[0].innerHTML,
+        tableBody.children[i].cells[10].innerHTML
+      ] = [
+        tableBody.children[i].cells[10].innerHTML,
+        tableBody.children[i].cells[0].innerHTML
+      ]; //swap the table body elements
+      tableBody.children[i].addEventListener("click", () => {
+        searchBox.value = tableBody.children[i].cells[0].innerText;
+        searchBox.focus();
+      });
     }
   }
 }
 
 function removeConfirmWaitFunction(data) {
-  if (document.querySelector(".link") != null && data.options.timeout) {
-    document.querySelector(".link").style.display = "inline";
-  }
+  document.querySelector(".link").style.display = "inline";
 }
 
 function autoCapitalizeFunction(data) {
-  if (
-    document.querySelector(
-      'input[name="repairDetails.repairOrderDetails[0].serialNumber"]'
-    ) &&
-    data.options.autoCapitalize
-  ) {
-    kgb = document.querySelector(
-      'input[name="repairDetails.repairOrderDetails[0].serialNumber"]'
-    );
-    kbb = document.querySelector(
-      'input[name="repairDetails.repairOrderDetails[0].oldSerialNumber"]'
-    );
-    kgb.value = kgb.value.toUpperCase();
-    kbb.value = kbb.value.toUpperCase();
-  }
-  if (
-    document.querySelector(
-      'input[name="repairDetails.repairHeaderDetails.newSerialNumber"]'
-    ) &&
-    data.options.autoCapitalize
-  ) {
-    ciar = document.querySelector(
-      'input[name="repairDetails.repairHeaderDetails.newSerialNumber"]'
-    );
-    ciar.value = ciar.value.toUpperCase();
-  }
+  kgb = document.querySelector(
+    'input[name="repairDetails.repairOrderDetails[0].serialNumber"]'
+  );
+  kbb = document.querySelector(
+    'input[name="repairDetails.repairOrderDetails[0].oldSerialNumber"]'
+  );
+  kgb.value = kgb.value.toUpperCase();
+  kbb.value = kbb.value.toUpperCase();
+}
+if (
+  document.querySelector(
+    'input[name="repairDetails.repairHeaderDetails.newSerialNumber"]'
+  ) &&
+  data.options.autoCapitalize
+) {
+  ciar = document.querySelector(
+    'input[name="repairDetails.repairHeaderDetails.newSerialNumber"]'
+  );
+  ciar.value = ciar.value.toUpperCase();
 }
 
 setInterval(() => getData(), 500);
